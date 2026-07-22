53fad589b4bf62e6cb1d5e431a27d993ba378ca7
ajay311517104001 <46015974+ajay311517104001@users.noreply.github.com>
2026-07-22 17:25:11 -0400

    Add: n-th-tribonacci-number - submission-4

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-4.py
@@ -0,0 +1,27 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        # cache = {}
+        # def solve(i):
+
+        #     if i == 0:
+        #         return 0
+        #     if i == 1 or i == 2:
+        #         return 1
+        #     if i in cache:
+        #         return cache[i]
+
+        #     cache[i] = solve(i-1) + solve(i-2) + solve(i-3)
+        #     return cache[i]
+        
+        # return solve(n)
+        if n == 0 :
+            return 0
+        if n == 1 or n == 2:
+            return 1
+        dp = [0,1,1]
+        for i in range(3,n+1):
+            temp = dp[0]+dp[1]+dp[2]
+            dp[0] = dp[1]
+            dp[1] = dp[2]
+            dp[2] = temp
+        return dp[2]
